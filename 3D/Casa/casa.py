1cf0a906adae035484555102d3f00b674dbaeace
Bernardo <bernardo@csu.fullerton.edu>
2018-04-21 16:02:36 -0300

    Melhorias no 3d da Casa, adicionado telhado e jardim

--- 3D/Casa/casa.py
+++ 3D/Casa/casa.py
@@ -23,7 +23,7 @@ dz = 0
  
 def LoadTextures():
     global texture
-    texture = glGenTextures(2)
+    texture = glGenTextures(4)
     ################################################################################
     glBindTexture(GL_TEXTURE_2D, texture[0])
     #img = Image.open('atras.png')
@@ -74,7 +74,58 @@ def LoadTextures():
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
     ################################################################################
- 
+	
+		
+    ################################################################################
+    glBindTexture(GL_TEXTURE_2D, texture[2])
+    #img = Image.open('frente.png')
+    reader = png.Reader(filename='telhado.png')
+    w, h, pixels, metadata = reader.read_flat()
+
+    #w, h = img.size[0], img.size[1]
+    #print w, h
+    #pixels = img.getdata()
+    # pixels = [pixels[i * w:(i + 1) * w] for i in xrange(h)]
+    #img_data = numpy.array(list(img.getdata()), numpy.uint8)
+    if(metadata['alpha']):
+		modo = GL_RGBA
+    else:
+        modo = GL_RGB
+    glPixelStorei(GL_UNPACK_ALIGNMENT,1)
+    glTexImage2D(GL_TEXTURE_2D, 0, modo, w, h, 0, modo, GL_UNSIGNED_BYTE, pixels.tolist())
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+    ################################################################################
+	
+    ################################################################################
+    glBindTexture(GL_TEXTURE_2D, texture[3])
+    #img = Image.open('frente.png')
+    reader = png.Reader(filename='chao.png')
+    w, h, pixels, metadata = reader.read_flat()
+
+    #w, h = img.size[0], img.size[1]
+    #print w, h
+    #pixels = img.getdata()
+    # pixels = [pixels[i * w:(i + 1) * w] for i in xrange(h)]
+    #img_data = numpy.array(list(img.getdata()), numpy.uint8)
+    if(metadata['alpha']):
+		modo = GL_RGBA
+    else:
+        modo = GL_RGB
+    glPixelStorei(GL_UNPACK_ALIGNMENT,1)
+    glTexImage2D(GL_TEXTURE_2D, 0, modo, w, h, 0, modo, GL_UNSIGNED_BYTE, pixels.tolist())
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
 def InitGL(Width, Height):             
     LoadTextures()
     glEnable(GL_TEXTURE_2D)
@@ -139,6 +190,7 @@ def DrawGLScene():
     glTexCoord2f(0.11, -0.6); glVertex3f(1.0,  1.0,  1.0)  	
     glTexCoord2f(0.49, -0.6); glVertex3f( -1.0, 1.0,  1.0) 
     glEnd()
+	
     glBindTexture(GL_TEXTURE_2D, texture[0])
 	#Telhado da casa(Atras)
     glBegin(GL_TRIANGLES)
@@ -146,6 +198,7 @@ def DrawGLScene():
     glTexCoord2f(0.5, -0.6); glVertex3f( -1.0, 1.0, -1.0) 
     glTexCoord2f(0.09, -0.6); glVertex3f(1.0,  1.0,  -1.0)  	
     glEnd()
+	
     glBindTexture(GL_TEXTURE_2D, texture[0])
     #Atras da casa
     glBegin(GL_QUADS)
@@ -155,41 +208,45 @@ def DrawGLScene():
     glTexCoord2f(0.49, -0.13); glVertex3f( 1.0, -1.0, -1.0)   
     glEnd() 
 	
-    glBindTexture(GL_TEXTURE_2D, texture[1])
+    glBindTexture(GL_TEXTURE_2D, texture[2])
 	#Telhado(lados esquerdo)
     glBegin(GL_QUAD_STRIP) 
-    glTexCoord2f(-0.11, -0.12); glVertex3f( 0.0, 2.0, 1.0) 
-    glTexCoord2f(-0.49, -0.12); glVertex3f( -1.0, 1.0, 1.0)
-    glTexCoord2f(-0.49, -0.6); glVertex3f( 0.0, 2.0, -1.0)
-    glTexCoord2f(-0.11, -0.6); glVertex3f( -1.0, 1.0, -1.0)	
+    glTexCoord2f(-0.50, -0.87); glVertex3f( 0.0, 2.0, 1.0) 
+    glTexCoord2f(-0.17, -0.84); glVertex3f( -1.0, 1.0, 1.0)
+    glTexCoord2f(-0.50, -0.12); glVertex3f( 0.0, 2.0, -1.0)
+    glTexCoord2f(-0.17, -0.15); glVertex3f( -1.0, 1.0, -1.0)	
     glEnd()
 	
+    glBindTexture(GL_TEXTURE_2D, texture[2])
 	#Telhado(lados direito)
     glBegin(GL_QUAD_STRIP)
-    glTexCoord2f(1.0, 0.0); glVertex3f( 0.0, 2.0, 1.0) 
-    glTexCoord2f(0.0, 0.0); glVertex3f(1.0,  1.0,  1.0)
-    glTexCoord2f(1.0, 0.0); glVertex3f( 0.0, 2.0, -1.0) 
-    glTexCoord2f(0.0, 0.0); glVertex3f(1.0,  1.0,  -1.0)  	
-    glEnd()
-	# Lateral da casa(Esquerda)
-    glBegin(GL_QUADS)
-    glTexCoord2f(-0.11, -0.12); glVertex3f(-1.0, -1.0, -1.0)  
-    glTexCoord2f(-0.49, -0.12); glVertex3f(-1.0, -1.0,  1.0)    
-    glTexCoord2f(-0.49, -0.6); glVertex3f(-1.0,  1.0,  1.0)   
-    glTexCoord2f(-0.11, -0.6); glVertex3f(-1.0,  1.0, -1.0)   
-    glEnd()
-
-	#Jardim/chao casa
-    glBegin(GL_QUADS)
-    glTexCoord2f(0.0, 0.0); glVertex3f(-5.0, -1.0,  5.0)  
-    glTexCoord2f(1.0, 0.0); glVertex3f(5.0,  -1.0,   5.0)    
-    glTexCoord2f(1.0, 1.0); glVertex3f(5.0, -1.0,  -5.0)   
-    glTexCoord2f(0.0, 1.0); glVertex3f(-5.0, -1.0, -5.0)
+    glTexCoord2f(0.50, -0.87); glVertex3f( 0.0, 2.0, 1.0) 
+    glTexCoord2f(0.17, -0.84); glVertex3f(1.0,  1.0,  1.0)
+    glTexCoord2f(0.50, -0.12); glVertex3f( 0.0, 2.0, -1.0) 
+    glTexCoord2f(0.17, -0.15); glVertex3f(1.0,  1.0,  -1.0)  	
     glEnd()
 	
-    xrot = xrot + 0.01                # X rotation
-    yrot = yrot + 0.01                 # Y rotation
-    zrot = zrot + 0.01                 # Z rotation
+    glBindTexture(GL_TEXTURE_2D, texture[1])
+	# Lateral da casa(Esquerda)
+    glBegin(GL_QUADS)
+    glTexCoord2f(-0.12, -0.12); glVertex3f(-1.0, -1.0, -1.0)  
+    glTexCoord2f(-0.49, -0.12); glVertex3f(-1.0, -1.0,  1.0)    
+    glTexCoord2f(-0.49, -0.6); glVertex3f(-1.0,  1.0,  1.0)   
+    glTexCoord2f(-0.12, -0.6); glVertex3f(-1.0,  1.0, -1.0)   
+    glEnd()
+	
+    glBindTexture(GL_TEXTURE_2D, texture[3])
+	#Jardim/chao casa
+    glBegin(GL_QUADS)
+    glTexCoord2f(0.0, 0.0); glVertex3f(-1.5, -1.0,  1.6)  
+    glTexCoord2f(-1.0, 0.0); glVertex3f(2.86,  -1.0,   1.6)    
+    glTexCoord2f(-1.0, -1.0); glVertex3f(2.86, -1.0,  -1.6)   
+    glTexCoord2f(0.0, -1.0); glVertex3f(-1.5, -1.0, -1.6)
+    glEnd()
+	
+    xrot = xrot + 0.06                # X rotation
+    yrot = yrot + 0.06                # Y rotation
+    zrot = zrot + 0.06                 # Z rotation
  
     glutSwapBuffers()
  
